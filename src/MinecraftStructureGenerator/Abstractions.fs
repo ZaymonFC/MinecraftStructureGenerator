d833fc01ee0724f8f0e55f35246c7d8c5b782f80
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2020-07-03 18:22:20 +1000

    ✨ Update abstractions

--- src/MinecraftStructureGenerator/Abstractions.fs
+++ src/MinecraftStructureGenerator/Abstractions.fs
@@ -4,9 +4,9 @@ open Model
 
 // Command should be enriched in the future - Type, relative vs absolute ... etc.
 type Command = Command of string
-type CommandStream = CommandStream of string
+type CommandStream = Command list
 
-type CommandCompiler = Blocks -> Command list
+type Comandify = Blocks -> Command list
 type CommandFormatter = Command list -> CommandStream
 
 type Writer = CommandStream -> unit IO
